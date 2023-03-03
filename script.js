32324749ecafb1fe67b6fab19b3e50714bd7b8a5
Tuan Anh <tunanguyenlc@gmail.com>
2023-03-03 16:09:16 +0700

    Bai4_BT1

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = 85;
+console.log("input: " + x);
+
+if (x <= 70) {
+    console.log("Grade D");
+} else if (x >= 71 && x <= 79) {
+    console.log("Grade C");
+} else if (x >= 80 && x <= 89) {
+    console.log("Grade B");
+} else if (x >= 90 && x <= 100) {
+    console.log("Grade A");
+}
+
